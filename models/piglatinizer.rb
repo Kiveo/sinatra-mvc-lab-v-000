089437bc9471f1216d9e7a097f8cbf7e859918af
Maverick Tesini <mavericktesini@gmail.com>
2018-07-14 06:11:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,42 +1,43 @@
-# class PigLatinizer
-#   attr_reader :words
-#
-#   def initialize
-#   end
-#
-#   def piglatinize(words)
-#     @words = words
-#   end
-#
-# end
-
 class PigLatinizer
-  attr_reader :text
+  attr_reader :words
 
-  def initialize
-
+  def initialize(words)
+    @words = words
   end
 
-  def piglatinize(text)
-    @text = text
-    aray = @text.split(" ")
-    new_aray = aray.collect do |word|
-      if word.slice(0,1) =~ (/[AEIOUaeiou]/)
-        word = word + "way"
-      else
-        word = word.slice(1,word.length) + word.slice(0,1)
-        if word.slice(0,1) =~ (/[aeiou]/)
-          word = word + "ay"
-        else
-          word = word.slice(1,word.length) + word.slice(0,1)
-          if word.slice(0,1) =~ (/[aeiou]/)
-            word = word + "ay"
-          else
-            word = word.slice(1,word.length) + word.slice(0,1) + "ay"
-          end
-        end
-      end
-    end
-   new_phrase = new_aray.join(" ")
+  def piglatinize(words)
+    @words = words
   end
+
 end
+
+# class PigLatinizer
+#   attr_reader :text
+# 
+#   def initialize
+# 
+#   end
+# 
+#   def piglatinize(text)
+#     @text = text
+#     aray = @text.split(" ")
+#     new_aray = aray.collect do |word|
+#       if word.slice(0,1) =~ (/[AEIOUaeiou]/)
+#         word = word + "way"
+#       else
+#         word = word.slice(1,word.length) + word.slice(0,1)
+#         if word.slice(0,1) =~ (/[aeiou]/)
+#           word = word + "ay"
+#         else
+#           word = word.slice(1,word.length) + word.slice(0,1)
+#           if word.slice(0,1) =~ (/[aeiou]/)
+#             word = word + "ay"
+#           else
+#             word = word.slice(1,word.length) + word.slice(0,1) + "ay"
+#           end
+#         end
+#       end
+#     end
+#    new_phrase = new_aray.join(" ")
+#   end
+# end
