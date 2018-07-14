29a59e775e6db30a69ac8e914be66f1ca1c5e404
Maverick Tesini <mavericktesini@gmail.com>
2018-07-14 06:51:09 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,8 +12,11 @@ class PigLatinizer
     #collect pig latin versions of words
     pig_words = split_words.map do |word|
       #find first vowel as an index of the array ([word] per iteration)
-      word.index("#{word.scan(/AEIOUaeiou/).first}") #first scanned vowel's index
-      #
+      vowel_index = word.index("#{word.scan(/AEIOUaeiou/).first}") #first scanned vowel's index
+      #slice! deletes and returns deleted content 
+      removed_portion = word.slice!(0,vowel_index) #remove form start until vowel (can => c)
+      #take remaining portion and add removed_portion to end + ay(way) depending on if first letter == vowel
+      word << "#{removed_portion}#{}ay" #ex1:egg=> eggway --ex2:dck => uck + d + ay => uckday 
     end
     #put pig words together as spaced text
     pig_words.join(" ")
