c8ca9114a6d7de1277609df50b2b8a4320b1cd9d
Maverick Tesini <mavericktesini@gmail.com>
2018-07-14 05:45:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinized' do
     #create a class instance of Model and pass user input to it
-    @pig_input = PigLatinizer.new(params[:user_input])
+    @pig_latin = PigLatinizer.new(params[:user_input])
     #route to erb page that utilizes class instance and methods
     erb :pig_latinized
   end
