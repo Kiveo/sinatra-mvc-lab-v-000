728ae35926c4d3c40b8d957131d3e2cdd6dc52f2
Maverick Tesini <mavericktesini@gmail.com>
2018-07-14 05:35:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,11 @@ class App < Sinatra::Base
     erb :user_input
   end
 
+  post '/piglatinized' do
+    @pig_input = params[:user_input]
+    #create a class instance of Model and pass user input to it
+    @pig_output = Pig
+    erb :piglatinized
+  end
+
 end
